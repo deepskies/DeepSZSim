2a10e2cdfa09180a708a1573a0a7357c106b3909
Brian-Zzzz <junhez3@uchicago.edu>
2022-07-07 17:48:36 -0400

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,7 +1,5 @@
 import numpy as np
-import os
-from scipy import interpolate
-from scipy.interpolate import UnivariateSpline, InterpolatedUnivariateSpline, interp1d
+from scipy.interpolate import interp1d
 from astropy.cosmology import FlatLambdaCDM
 from colossus.cosmology import cosmology
 from colossus.halo import mass_adv
@@ -40,7 +38,9 @@ def cosmo_para():
         omega_m = cosmo['Omega_m0']
         omega_b = cosmo['Omega_b0']
         h = cosmo['cosmo_h']
-    return omega_m, omega_b, h
+        sigma8 = cosmo['sigma8']
+        ns = cosmo['ns']
+    return omega_m, omega_b, h, sigma8, ns
 
 # Load telescope parameters from the config.yaml file
 def tele_para(tele, fre):
@@ -53,45 +53,42 @@ def tele_para(tele, fre):
         noise_level = telescope_f['noise_level']
     return beam_size, noise_level
 
-def rho_cri(z, cosmo_h, omega_m):
-    '''
-    Return Unit: msolar / MPC
-    '''
-    a = 1. + z
-    omega_d = 1 - omega_m
-    H_0 = cosmo_h * 100 / (3.09e19)        #Unit of 1/s
-    return (3 * H_0 ** 2)/(8 * np.pi * G) * (omega_m * a ** 3 + omega_d) / m_sun * Mpc_to_m ** 3
-
-def battaglia_profile(r, Mvir, z, rho_critical, omega_b0, omega_m0, cosmo_h):
+def battaglia_profile(r, Mvir, z):
     '''
     Using Battaglia et al (2012). Eq. 10. 
-    Input: M200 in solar mass and R200 in Mpc
+    Input: Virial Mass in solar mass and Radius in Mpc
     Return: Pressure profile in keV/cm^3 at radius r
     '''
-    params = {'flat': True, 'H0': 70, 'Om0': 0.27, 'Ob0': 0.044, 'sigma8': 0.80, 'ns': 0.95}
+    omega_m0, omega_b0, cosmo_h, sigma8, ns = cosmo_para()
+
+    a = 1. + z
+    omega_d0 = 1 - omega_m0
+    H_0 = cosmo_h * 100 / (Mpc_to_m/1000) 
+    rho_critical = (3 * H_0 ** 2)/(8 * np.pi * G) * (omega_m0 * a ** 3 + omega_d0) / m_sun * Mpc_to_m ** 3       #msolar / MPC^3
+
+    params = {'flat': True, 'H0': cosmo_h*100, 'Om0': omega_m0, 'Ob0': omega_b0, 'sigma8':sigma8, 'ns': ns}
     cosmology.addCosmology('myCosmo', **params)
     cosmo = cosmology.setCosmology('myCosmo')
     #Option to customize concentration, currently default, using Bullock et al. (2001)
     #cvir = concentration.concentration(Mvir, 'vir', z, model = 'ishiyama21')      #Ishiyama et al. (2021)
-    M200, R200, c200 = mass_adv.changeMassDefinitionCModel(Mvir/cosmo_h, z, 'vir', '200c')
+    M200, R200, c200 = mass_adv.changeMassDefinitionCModel(Mvir/cosmo_h, z, 'vir', '200c', c_model = 'ishiyama21')
     M200 *= cosmo_h
     R200 = R200 / 1000 * cosmo_h
     
     R200 *= (1. + z)                            # Proper distance to Comoving distance
     x = r / R200
-    msolar = 1.9889e30                          # kg
-    mpc2cm = 3.0856e24
-    G = 4.3e-9                                  # Mpc Mo^-1 (km/s)^2 
-    alpha = 1.0
+    G2 = G * 1e-6 / Mpc_to_m * m_sun                               # Mpc Msun^-1 (km/s)^2 
     gamma = -0.3
-    P200 = 200. * rho_critical * omega_b0 * G * M200 / omega_m0 / 2. / (R200 / (1. + z))    # Msun km^2 / Mpc^3 / s^2
+    P200 =  G2 * M200 * 200. * rho_critical * (omega_b0 / omega_m0) / 2. / (R200 / (1. + z))    # Msun km^2 / s^2 / Mpc^3
 
     P0 = 18.1 * ((M200 / 1e14)**0.154 * (1. + z)**-0.758)
     xc = 0.497 * ((M200 / 1e14)**-0.00865 * (1. + z)**0.731)
     beta = 4.35 * ((M200 / 1e14)**0.0393 * (1. + z)**0.415) 
-    pth = P200 * P0 * (x / xc)**gamma * (1. + (x/xc))**(-1. * beta)      # Msun km^2 / Mpc^3 / s^2
+    pth = P200 * P0 * (x / xc)**gamma * (1. + (x/xc))**(-1. * beta)      # Msun km^2 / s^2 / Mpc^3
 
-    pth *= (msolar * 6.24e15 * 1e6 / mpc2cm**3)       # keV/cm^3
+    j_to_kev = 6.242e15
+
+    pth *= (m_sun * 1e6 * j_to_kev  / ((Mpc_to_m*100)**3))       # keV/cm^3
     p_e = pth * 0.518       # Vikram et al (2016)
     return p_e
 
@@ -137,23 +134,17 @@ def Mpc_to_arcmin(r, z):
     Input: distance r, redshift
     Return: angular scale
     '''
-    cosmo = FlatLambdaCDM(H0=70, Om0=0.27)
+    omega_m0, omega_b0, cosmo_h, sigma8, ns = cosmo_para()
+    cosmo = FlatLambdaCDM(H0=cosmo_h*100, Om0=omega_m0)
     Kpc_per_arcmin = cosmo.kpc_comoving_per_arcmin(z).value
     Mpc_per_arcmin = Kpc_per_arcmin/1000
     return r / Mpc_per_arcmin
 
-
-def convolve_map_with_gaussian_beam(pix_size,beam_size_fwhp,Map):
-    gaussian = make_2d_gaussian_beam(pix_size,beam_size_fwhp)
-  
-    FT_gaussian = np.fft.fft2(np.fft.fftshift(gaussian)) # first add the shift so that it is central
-    FT_Map = np.fft.fft2(np.fft.fftshift(Map)) #shift the map too
-    convolved_map = np.fft.fftshift(np.real(np.fft.ifft2(FT_gaussian*FT_Map))) 
-    
-    return(convolved_map)
-  ###############################   
-
 def make_2d_gaussian_beam(pix_size,beam_size_fwhp):
+    '''
+    Input: pixel size, beam size in arcmin
+    Return: gaussian beam
+    '''
     N=101
     ones = np.ones(N)
     inds  = (np.arange(N)+.5 - N/2.) * pix_size
@@ -165,6 +156,19 @@ def make_2d_gaussian_beam(pix_size,beam_size_fwhp):
     gaussian = np.exp(-.5 *(R/beam_sigma)**2.)
     gaussian = gaussian / np.sum(gaussian)
     return(gaussian)
+
+def convolve_map_with_gaussian_beam(pix_size, beam_size_fwhp, Map):
+    '''
+    Input: pixel size, beam size in arcmin, image
+    Return: convolved map
+    '''
+    gaussian = make_2d_gaussian_beam(pix_size, beam_size_fwhp)
+  
+    FT_gaussian = np.fft.fft2(np.fft.fftshift(gaussian))
+    FT_Map = np.fft.fft2(np.fft.fftshift(Map))
+    convolved_map = np.fft.fftshift(np.real(np.fft.ifft2(FT_gaussian * FT_Map))) 
+    
+    return(convolved_map)
 
 def plot_img(image, z, mode = 1, cmb = 0, save = False, path = None):
     '''
@@ -218,30 +222,44 @@ def plot_y(r, y, z):
     ax[1].title.set_text("Y(Log) z="+str(z))
     plt.show()
     
-def generate_img(radius, profile, f, noise_level, beam_size, z, plain_y = False, Mpc5 = False, Mpc = False, cmb = False, cmb_n = False, y_n = False, s = False, p = None):
-    if plain_y:
+def generate_img(radius, profile, f, noise_level, beam_size, z, option, s = False, p = None):
+    if option == 1:
         plot_y(radius, profile, z)
-    if Mpc5:
+    if option == 2:
         log_image = make_proj_image_new(radius,np.log10(profile),maxRadius=5,pixel_scale=0.05,extrapolate=True)
         plot_img(log_image, z, mode = 0, save = s, path = p)
     
     y_img = make_proj_image_new(radius,profile,extrapolate=True)
+    if option == 3:
+        plot_img(y_img, z, save = s, path = p)
+    
+    if option == 4:
+        gaussian = make_2d_gaussian_beam(Mpc_to_arcmin(0.005, z), beam_size)
+        if s:
+            fig, ax = plt.subplots(1,1)
+            img = ax.imshow(gaussian)
+            fig.colorbar(img)
+            plt.savefig(p)
+    y_con = convolve_map_with_gaussian_beam(Mpc_to_arcmin(0.005, z), beam_size , y_img)
+    
     
     t_cmb = 2.725            #K
     fsz = f_sz(f, t_cmb)
-    cmb_img = y_img * fsz * t_cmb * 1e6
+    cmb_img = y_con * fsz * t_cmb * 1e6
     
     noise = np.random.normal(0, 1, (101,101)) * noise_level
+    if option == 7:
+        plot_img(noise, z, cmb = 1, save = s, path = p)
+
     CMB_noise = cmb_img + noise
     
     y_noise = CMB_noise / fsz / t_cmb / 1e6
     
-    if Mpc:
-        plot_img(y_img, z, save = s, path = p)
-    if cmb:
+    if option == 5:
+        plot_img(y_con, z, save = s, path = p)
+    if option == 6:
         plot_img(cmb_img, z, cmb = 1, save = s, path = p)
-    if cmb_n:
+    if option == 8:
         plot_img(CMB_noise, z, cmb = 1, save = s, path = p)
-    if y_n:
-        final = convolve_map_with_gaussian_beam(Mpc_to_arcmin(0.005, z), beam_size , y_noise)
-        plot_img(final, z, save = s, path = p)
+    if option == 9:
+        plot_img(y_noise, z, save = s, path = p)
