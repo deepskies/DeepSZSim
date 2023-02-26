818494dbcf0cf40dd675bda2f0f0a9620df41529
Brian Nord <184985+bnord@users.noreply.github.com>
2023-02-25 19:27:20 -0500

    created a union of simulation files. deleted redundant one.

--- simulation.py
+++ simulation.py
@@ -9,8 +9,6 @@ import matplotlib as mpl
 mpl.style.use('default')
 
 
-
-
 def battaglia_profile(r, Mvir, z):
     '''
     Using Battaglia et al (2012). Eq. 10. 
@@ -30,11 +28,13 @@ def battaglia_profile(r, Mvir, z):
     #Option to customize concentration, currently default, using Bullock et al. (2001)
     #cvir = concentration.concentration(Mvir, 'vir', z, model = 'ishiyama21')      #Ishiyama et al. (2021)
     M200, R200, c200 = mass_adv.changeMassDefinitionCModel(Mvir/cosmo_h, z, 'vir', '200c', c_model = 'ishiyama21')
+    M200, R200, c200 = mass_adv.changeMassDefinitionCModel(Mvir/cosmo_h, z, 'vir', '200c', c_model = 'ishiyama21')
     M200, R200, c200 = mass_adv.changeMassDefinitionCModel(M500/cosmo_h, z, '500c', '200c', c_model = 'ishiyama21')
     M200 *= cosmo_h
     R200 = R200 / 1000 * cosmo_h
     
     R200 *= (1. + z)                            # Proper distance to Comoving distance
+    x = r / R200
     G2 = G * 1e-6 / Mpc_to_m * m_sun                               # Mpc Msun^-1 (km/s)^2 
     gamma = -0.3
     P200 =  G2 * M200 * 200. * rho_critical * (omega_b0 / omega_m0) / 2. / (R200 / (1. + z))    # Msun km^2 / s^2 / Mpc^3
@@ -42,7 +42,6 @@ def battaglia_profile(r, Mvir, z):
     P0 = 18.1 * ((M200 / 1e14)**0.154 * (1. + z)**-0.758)
     xc = 0.497 * ((M200 / 1e14)**-0.00865 * (1. + z)**0.731)
     beta = 4.35 * ((M200 / 1e14)**0.0393 * (1. + z)**0.415) 
-    x = r / R200
     pth = P200 * P0 * (x / xc)**gamma * (1. + (x/xc))**(-1. * beta)      # Msun km^2 / s^2 / Mpc^3
 
     j_to_kev = 6.242e15
@@ -50,8 +49,8 @@ def battaglia_profile(r, Mvir, z):
     pth *= (m_sun * 1e6 * j_to_kev  / ((Mpc_to_m*100)**3))       # keV/cm^3
     p_e = pth * 0.518       # Vikram et al (2016)
     #return p_e
+    #return p_e
     return p_e, M200, R200, c200
-
 
 def epp_to_y(profile):
     '''
@@ -139,115 +138,6 @@ def convolve_map_with_gaussian_beam(pix_size, beam_size_fwhp, Map):
     
     return(convolved_map)
 
-def generate_img(radius, profile, f, noise_level, beam_size, z, nums, p = None, AP = False):
-    if 1 in nums:
-        pa = p + '1' + '.png'
-        plot_y(radius, profile, z, pa)
-    
-    y_img = make_proj_image_new(radius,profile,extrapolate=True)
-
-    if 2 in nums:
-        pa = p + '2' + '.png'
-        plot_img(y_img, z, path = pa)
-    
-    if 3 in nums:
-        pa = p + '3' + '.png'
-        gaussian = gaussian_kernal(0.5, beam_size)
-        plot_img(gaussian, z, opt = 2, path = pa)
-
-    y_con = convolve_map_with_gaussian_beam(0.5, beam_size , y_img)
-    
-    
-    t_cmb = 2.725            #K
-    fsz = f_sz(f, t_cmb)
-    #2d array of convolved submap (Temperature Decrement)
-    cmb_img = y_con * fsz * t_cmb * 1e6
-    
-    #2d array of the noise
-    noise = np.random.normal(0, 1, (37, 37)) * noise_level
-
-    #Temperature Decrement with noise
-    CMB_noise = cmb_img + noise
-    
-    y_noise = CMB_noise / fsz / t_cmb / 1e6
-    
-    if 4 in nums:
-        pa = p + '4' + '.png'
-        plot_img(y_con, z, path = pa)
-    if 5 in nums:
-        pa = p + '5' + '.png'
-        plot_img(cmb_img, z, opt = 1, path = pa)
-    if 6 in nums:
-        pa = p + '6' + '.png'
-        plot_img(noise, z, opt = 1, path = pa)
-    if 7 in nums:
-        pa = p + '7' + '.png'
-        plot_img(CMB_noise, z, opt = 1, path = pa)
-    if 8 in nums:
-        pa = p + '8' + '.png'
-        plot_img(y_noise, z, path = pa)
-    if 9 in nums:
-        pa = p + '9' + '.png'
-        plot_img(y_noise, z, opt = 3, path = pa) #vizualization starts working from z = 0.115
-
-    if AP:
-        print("tSZ Signal: " + str(tSZ_signal(z, y_noise)))
-
-    return y_img, y_con, cmb_img, noise, cmb_noise, y_noise, SZsignal, aperture    
-    
-
-
-def tSZ_signal(z, Map):
-    # https://kbarbary-astropy.readthedocs.io/en/latest/_modules/astropy/cosmology/funcs.html#kpc_proper_per_arcmin
-    omega_m0, omega_b0, cosmo_h, sigma8, ns = cosmo_para()
-    cosmo = FlatLambdaCDM(H0=cosmo_h*100, Om0=omega_m0)
-    angular_dd_z = cosmo.angular_diameter_distance(z).value * np.pi / 10800.0 
-    angular_dd = cosmo.angular_diameter_distance(0.5).value * np.pi / 10800.0
-    rin = 2.1 * angular_dd_z / angular_dd # Calafut et al 2021
-    rout = np.sqrt(2) * rin
-    
-    image_size = 37
-    pixel_scale = 0.5
-    x,y=np.meshgrid(np.arange(image_size),np.arange(image_size))
-    r = np.sqrt((x-image_size//2)**2+(y-image_size//2)**2)*pixel_scale
-
-    # tSZ signal calculation
-    disk_mean = Map[r < rin].mean()
-    ring_mean = Map[(r >= rin) & (r < rout)].mean()
-    tSZ = disk_mean - ring_mean
-    
-    return tSZ
-
-def radius_size(z, disk = False, ring = False):
- 
-    omega_m0, omega_b0, cosmo_h, sigma8, ns = cosmo_para()
-    cosmo = FlatLambdaCDM(H0=cosmo_h*100, Om0=omega_m0)
-    angular_dd_z = cosmo.angular_diameter_distance(z).value * np.pi / 10800.0 
-    angular_dd = cosmo.angular_diameter_distance(0.5).value * np.pi / 10800.0
-    rin = 2.1 * angular_dd_z / angular_dd
-    rout = np.sqrt(2) * rin
-    
-    image_size = 37
-    pixel_scale = 0.5
-    x,y=np.meshgrid(np.arange(image_size),np.arange(image_size))
-    r = np.sqrt((x-image_size//2)**2+(y-image_size//2)**2)*pixel_scale
-
-    if disk:
-        rows, columns = np.where(r < rin)
-        value = image_size//2 - rows[0]
-        return value
-    
-    if ring:
-        rows, columns = np.where(r < rout)
-        value = image_size//2 - rows[0]
-        return value
-
-    
-
-
-
-
-
 def plot_img(image, z, opt = 0, path = None):
     '''
     Input: image, mode (option of 0.5/5 Mpc, default to 0.5), cmb (option of y/delta_T, default to y)
@@ -282,8 +172,8 @@ def plot_img(image, z, opt = 0, path = None):
     plt.ylabel(r'Mpc')
 
     if opt == 3:
-        circle_disk = plt.Circle((18, 18), radius_size(z,disk = True), color='white', fill=False, linewidth=2)
-        circle_ring = plt.Circle((18, 18), radius_size(z,ring = True), color='aqua', fill=False, linewidth=2)
+        circle_disk = plt.Circle((18, 18), radius_size(z,disk = True), color='green', fill=False, linewidth=2)
+        circle_ring = plt.Circle((18, 18), radius_size(z,ring = True), color='black', fill=False, linewidth=2)
         ax.add_patch(circle_disk)
         ax.add_patch(circle_ring)
 
@@ -305,4 +195,100 @@ def plot_y(r, y, z, path):
     ax[1].set_ylabel(r'Mpc$^{-1}$')
     ax[1].title.set_text("Y(Log) z="+str(z))
     plt.savefig(path)
+    
+def generate_img(radius, profile, f, noise_level, beam_size, z, nums, p = None):
+    if 1 in nums:
+        pa = p + '1' + '.png'
+        plot_y(radius, profile, z, pa)
+    
+    y_img = make_proj_image_new(radius,profile,extrapolate=True)
+
+    if 2 in nums:
+        pa = p + '2' + '.png'
+        plot_img(y_img, z, path = pa)
+    
+    if 3 in nums:
+        pa = p + '3' + '.png'
+        gaussian = gaussian_kernal(0.5, beam_size)
+        plot_img(gaussian, z, opt = 2, path = pa)
+
+    y_con = convolve_map_with_gaussian_beam(0.5, beam_size , y_img)
+    
+    
+    t_cmb = 2.725            #K
+    fsz = f_sz(f, t_cmb)
+    cmb_img = y_con * fsz * t_cmb * 1e6
+    
+    noise = np.random.normal(0, 1, (37, 37)) * noise_level
+    CMB_noise = cmb_img + noise
+    
+    y_noise = CMB_noise / fsz / t_cmb / 1e6
+    
+    if 4 in nums:
+        pa = p + '4' + '.png'
+        plot_img(y_con, z, path = pa)
+    if 5 in nums:
+        pa = p + '5' + '.png'
+        plot_img(cmb_img, z, opt = 1, path = pa)
+    if 6 in nums:
+        pa = p + '6' + '.png'
+        plot_img(noise, z, opt = 1, path = pa)
+    if 7 in nums:
+        pa = p + '7' + '.png'
+        plot_img(CMB_noise, z, opt = 1, path = pa)
+    if 8 in nums:
+        pa = p + '8' + '.png'
+        plot_img(y_noise, z, path = pa)
+    if 9 in nums:
+        pa = p + '9' + '.png'
+        plot_img(y_noise, z, opt = 3, path = pa) #vizualization starts working from z = 0.115
+
+    return tSZ_signal(z, y_con), tSZ_signal(z, y_noise)
+
+def tSZ_signal(z, Map):
+    # https://kbarbary-astropy.readthedocs.io/en/latest/_modules/astropy/cosmology/funcs.html#kpc_proper_per_arcmin
+    omega_m0, omega_b0, cosmo_h, sigma8, ns = cosmo_para()
+    cosmo = FlatLambdaCDM(H0=cosmo_h*100, Om0=omega_m0)
+    angular_dd_z = cosmo.angular_diameter_distance(z).value * np.pi / 10800.0 
+    angular_dd = cosmo.angular_diameter_distance(0.5).value * np.pi / 10800.0
+    rin = 2.1 * angular_dd_z / angular_dd # Calafut et al 2021
+    rout = np.sqrt(2) * rin
+    
+    image_size = 37
+    pixel_scale = 0.5
+    x,y=np.meshgrid(np.arange(image_size),np.arange(image_size))
+    r = np.sqrt((x-image_size//2)**2+(y-image_size//2)**2)*pixel_scale
+
+    # tSZ signal calculation
+    disk_mean = Map[r < rin].mean()
+    ring_mean = Map[(r >= rin) & (r < rout)].mean()
+    tSZ = disk_mean - ring_mean
+    
+    return tSZ
+
+def radius_size(z, disk = False, ring = False):
+ 
+    omega_m0, omega_b0, cosmo_h, sigma8, ns = cosmo_para()
+    cosmo = FlatLambdaCDM(H0=cosmo_h*100, Om0=omega_m0)
+    angular_dd_z = cosmo.angular_diameter_distance(z).value * np.pi / 10800.0 
+    angular_dd = cosmo.angular_diameter_distance(0.5).value * np.pi / 10800.0
+    rin = 2.1 * angular_dd_z / angular_dd
+    
+    rout = np.sqrt(2) * rin
+    
+    image_size = 37
+    pixel_scale = 0.5
+    x,y=np.meshgrid(np.arange(image_size),np.arange(image_size))
+    r = np.sqrt((x-image_size//2)**2+(y-image_size//2)**2)*pixel_scale
+
+    if disk:
+        rows, columns = np.where(r < rin)
+        value = image_size//2 - rows[0]
+        return value
+    
+    if ring:
+        rows, columns = np.where(r < rout)
+        value = image_size//2 - rows[0]
+        return value
+
     
