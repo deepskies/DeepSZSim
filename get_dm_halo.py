7892188eb76d1b1576e3f49fb2e8345903ab32b2
Kush Banker <kushbanker@gmail.com>
2023-06-23 13:45:32 +0100

    Re-added vir_to_200 mass def change method to halo

--- get_dm_halo.py
+++ get_dm_halo.py
@@ -1,4 +1,6 @@
 import numpy as np
+from colossus.cosmology import cosmology
+from colossus.halo import mass_adv
 
 class GenerateHalo():
 
@@ -11,3 +13,17 @@ class GenerateHalo():
         
         return zdist, mdist
         
+    def vir_to_200_colossus(self,cosmo,sigma8,ns,Mvir,z):
+
+
+         params = {'flat': True, 'H0': cosmo.H0.value, 'Om0': cosmo.Om0, 'Ob0': cosmo.Ob0, 'sigma8':sigma8, 'ns': ns}
+         cosmology.addCosmology('myCosmo', **params)
+         cosmo_colossus= cosmology.setCosmology('myCosmo')
+
+         M200, R200, c200 = mass_adv.changeMassDefinitionCModel(Mvir/cosmo.h, z, 'vir', '200c', c_model = 'ishiyama21')
+
+         M200 *= cosmo.h #From M_solar/h to M_solar
+         R200 = R200*cosmo.h/1000 #From kpc/h to Mpc
+         R200 = R200/cosmo.scale_factor(z) #From Mpc proper to Mpc comoving
+
+         return (M200,R200,c200)
