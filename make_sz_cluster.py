af8c58fed200d17f5d49b2ba83b1e8c988f2402a
Kush Banker <kushbanker@gmail.com>
2023-07-26 13:31:48 +0100

    Added comments/docstrings

--- make_sz_cluster.py
+++ make_sz_cluster.py
@@ -222,12 +222,20 @@ class GenerateCluster():
         #return tSZ_signal(z, y_con), tSZ_signal(z, y_noise)
         return y_img, y_con, cmb_img, noise, cmb_noise, y_noise, SZsignal, aperture
     
-    def get_cls(self, ns, cosmo):
-        data = camb.set_params(ns=ns, H0=cosmo.H0.value, ombh2=cosmo.Ob0, omch2=cosmo.Om0, lmax=2000,
+    def get_cls(self, ns, cosmo, lmax=2000):
+        '''
+        Makes a cmb temperature map based on the given power spectrum
+        Parameters:
+        ns, scalar spectral index of the power-spectrum
+        cosmo, background cosmology 
+
+        Return: power spectrum as can be used in szluster.make_cmb_map
+        '''
+        data = camb.set_params(ns=ns, H0=cosmo.H0.value, ombh2=cosmo.Ob0, omch2=cosmo.Om0, lmax=lmax,
                      WantTransfer=True)
         results = camb.get_results(data)
-        ps = np.zeros((3,3,2051))
         cls = np.swapaxes(results.get_total_cls(CMB_unit='muK', raw_cl=True),0,1)
+        ps = np.zeros((3,3,cls.shape[1]))
         # Needs to be reshaped to match input for pixell.enmap
         ps[0][0]= cls[0] # clTT spectrum
         ps[1][0] = cls[3] #clTE
@@ -237,6 +245,15 @@ class GenerateCluster():
         return ps
 
     def make_cmb_map(self, shape, pix_size, ps):
+        '''
+        Makes a cmb temperature map based on the given power spectrum
+        Parameters:
+        shape, shape of submap in arcmin
+        pix_size, size of each pixel in arcmin
+        ps, power spectrum with shape (3, 3, lmax); clTT spectrum at ps[0][0]
+
+        Return: cmb T map
+        '''
         #ps[0][0] is cltt spectrum
         shape,wcs = enmap.geometry(shape=shape,pos=(0,0),res=np.deg2rad(pix_size/60.))
         shape = (3,) + shape
