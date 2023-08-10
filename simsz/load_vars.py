382ffa59940a0e9757b362e866f1e0a6dfed12c5
Elaine Ran <elaineran@macbook-air-4.mynetworksettings.com>
2023-08-10 12:00:21 -0400

    removed read_yaml and make load_vars call read_yaml

--- simsz/load_vars.py
+++ simsz/load_vars.py
@@ -1,34 +1,27 @@
 import astropy
 from astropy.cosmology import FlatLambdaCDM
+import simsz.read_yaml as read_yaml
+from typing import Union
 
 class load_vars(object):
-    
-    def __init__(self):
+
+    def __init__(self, file_path= Union[None, str]):
+        self.file_path = file_path
         self.data = []
-        
-    def make_dict(self, ref):
-        
+
+    def make_dict_and_flatLCDM(self):
+        ref=read_yaml.YAMLOperator(self.file_path).parse_yaml()
         survey=[key for key in ref['SURVEYS'].keys()][0]
         survey_freq=[key for key in ref['SURVEYS'][survey].keys()][0]
         beam_size=ref['SURVEYS'][survey][survey_freq]['beam_size']
         noise_level=ref['SURVEYS'][survey][survey_freq]['noise_level']
         image_size = ref['IMAGES']['image_size']
         pixel_scale = ref['IMAGES']['pixel_scale']
-            
-        return(survey,survey_freq,beam_size,noise_level,image_size,pixel_scale)
-    
-    def flatLCDM(self, ref):
-        
         for key in ref['COSMOLOGY'].keys():
             cosmo_dict=ref['COSMOLOGY'][key] #Read in cosmological parameters
-        
+
         sigma8=cosmo_dict['sigma8']
         ns=cosmo_dict['ns']
-    
-        cosmo=FlatLambdaCDM(cosmo_dict['H0'], cosmo_dict['Omega_m0'], Tcmb0=cosmo_dict['t_cmb'], Ob0=cosmo_dict['Omega_b0']) 
-        
-        return (cosmo,sigma8,ns)
-    
-    
 
-    
+        cosmo=FlatLambdaCDM(cosmo_dict['H0'], cosmo_dict['Omega_m0'], Tcmb0=cosmo_dict['t_cmb'], Ob0=cosmo_dict['Omega_b0'])
+        return(survey,survey_freq,beam_size,noise_level,image_size,pixel_scale,cosmo,sigma8,ns)
