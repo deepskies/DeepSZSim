45ec8ced108f38548ce33443dc2ca83ff6254b17
samueldmcdermott <samueldmcdermott@gmail.com>
2023-10-26 10:51:02 -0400

    cleaned up `load_vars.py`

--- simsz/load_vars.py
+++ simsz/load_vars.py
@@ -7,33 +7,50 @@ import os
 class load_vars:
 
     def __init__(self, 
-                 file_path=os.path.join(os.path.dirname(__file__), "Settings", "inputdata.yaml")):
+                 file_path=os.path.join(os.path.dirname(__file__), "Settings", "inputdata.yaml"), survey_num = None,
+                 survey_name = None,
+                 survey_freq_val = None,
+                 cosmo_name = None):
         self.data = []
         self.file_path = file_path
+        self.dict = {}
+        ref = read_yaml.YAMLOperator(self.file_path).parse_yaml()
+        if len(list(ref['SURVEYS'].keys())) == 1:
+            survey = list(ref['SURVEYS'].keys())[0]
+        elif survey_num is not None:
+            survey = list(ref['SURVEYS'].keys())[survey_num]
+        elif survey_name is not None:
+            survey = ref['SURVEYS'][survey_name]
+        else:
+            print("specify a survey of interest with `survey_name`")
+            break
+        if len(list(ref['SURVEYS'][survey].keys())) == 1:
+            survey_freq = list(ref['SURVEYS'][survey].keys())[0]
+        elif survey_freq_val is not None:
+            survey_freq = ref['SURVEYS'][survey][survey_freq_val]
+        else:
+            print("specify a survey frequency of interest with `survey_freq_val`")
+            break
+        self.dict["survey"] = survey
+        self.dict["survey_freq"] = survey_freq
 
-    def make_dict_and_flatLCDM(self):
-        d = {}
-        ref=read_yaml.YAMLOperator(self.file_path).parse_yaml()
-        survey = [key for key in ref['SURVEYS'].keys()][0]
-        survey_freq = [key for key in ref['SURVEYS'][survey].keys()][0]
-        beam_size_arcmin = ref['SURVEYS'][survey][survey_freq]['beam_size']
-        noise_level = ref['SURVEYS'][survey][survey_freq]['noise_level']
-        image_size_arcmin = ref['IMAGES']['image_size']
-        pix_size_arcmin = ref['IMAGES']['pix_size']
-        d["survey"] = survey
-        d["survey_freq"] = survey_freq
-        d["beam_size_arcmin"] = beam_size_arcmin
-        d["noise_level"] = noise_level
-        d["image_size_arcmin"] = image_size_arcmin
-        d["pix_size_arcmin"] = pix_size_arcmin
-        for key in ref['COSMOLOGY'].keys():
-            cosmo_dict=ref['COSMOLOGY'][key] #Read in cosmological parameters
-
-        sigma8=cosmo_dict['sigma8']
-        ns=cosmo_dict['ns']
-
-        cosmo=FlatLambdaCDM(cosmo_dict['H0'], cosmo_dict['Omega_m0'], Tcmb0=cosmo_dict['t_cmb'], Ob0=cosmo_dict['Omega_b0'])
-        d["sigma8"] = sigma8
-        d["ns"] = ns
-        d["cosmo"] = cosmo
-        return d
+        self.dict["beam_size_arcmin"] = self.dict["survey"][survey_freq]['beam_size']
+        self.dict["noise_level"] = self.dict["survey"][survey_freq]['noise_level']
+        self.dict["image_size_arcmin"] = ref['IMAGES']['image_size']
+        self.dict["pix_size_arcmin"] = ref['IMAGES']['pix_size']
+        if len(list(ref['COSMOLOGY'].keys())) == 1:
+            cosmo_dict = ref['COSMOLOGY'][list(ref['COSMOLOGY'].keys())[0]] #Read in cosmological parameters
+        elif cosmo_name is not None:
+            cosmo_dict = ref['COSMOLOGY'][cosmo_name]
+        else:
+            print("specify cosmology name with `cosmo_name`")
+            break
+        if cosmo_dict['flat']:
+            self.dict["cosmo"] = FlatLambdaCDM(cosmo_dict['H0'], cosmo_dict['Omega_m0'], Tcmb0 = cosmo_dict['t_cmb'],
+                                               Ob0 = cosmo_dict['Omega_b0'])
+        else:
+            print("only flat cosmology supported at this time")
+            self.dict["cosmo"] = FlatLambdaCDM(cosmo_dict['H0'], cosmo_dict['Omega_m0'], Tcmb0 = cosmo_dict['t_cmb'],
+                                               Ob0 = cosmo_dict['Omega_b0'])
+        self.dict["sigma8"] = cosmo_dict['sigma8']
+        self.dict["ns"] = cosmo_dict['ns']
