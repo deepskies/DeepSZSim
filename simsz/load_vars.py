47f3deb8fecd8958cff9ff059314b8d843347030
samueldmcdermott <samueldmcdermott@gmail.com>
2023-10-26 11:42:13 -0400

    fixed more bugs in `load_vars.py`

--- simsz/load_vars.py
+++ simsz/load_vars.py
@@ -8,10 +8,11 @@ import sys
 class load_vars:
 
     def __init__(self, 
-                 file_path=os.path.join(os.path.dirname(__file__), "Settings", "inputdata.yaml"), survey_num = None,
-                 survey_name = None,
-                 survey_freq_val = None,
-                 cosmo_name = None):
+                 file_path = os.path.join(os.path.dirname(__file__), "Settings", "inputdata.yaml"),
+                 survey_num : int = None,
+                 survey_name : str = None,
+                 survey_freq_val : int = None,
+                 cosmo_name : str = None):
         self.data = []
         self.file_path = file_path
         self.dict = {}
@@ -21,20 +22,19 @@ class load_vars:
         elif survey_num is not None:
             survey = list(ref['SURVEYS'].keys())[survey_num]
         elif survey_name is not None:
-            survey = ref['SURVEYS'][survey_name]
+            survey = survey_name
         else:
-            print("specify a survey of interest with `survey_name`")
+            print("specify a survey of interest with `survey_name` or `survey_num`")
             sys.exit()
         if len(list(ref['SURVEYS'][survey].keys())) == 1:
             survey_freq = list(ref['SURVEYS'][survey].keys())[0]
         elif survey_freq_val is not None:
-            survey_freq = ref['SURVEYS'][survey][survey_freq_val]
+            survey_freq = survey_freq_val
         else:
             print("specify a survey frequency of interest with `survey_freq_val`")
             sys.exit()
         self.dict["survey"] = survey
         self.dict["survey_freq"] = survey_freq
-
         self.dict["beam_size_arcmin"] = ref['SURVEYS'][survey][survey_freq]['beam_size']
         self.dict["noise_level"] = ref['SURVEYS'][survey][survey_freq]['noise_level']
         self.dict["image_size_arcmin"] = ref['IMAGES']['image_size']
