b1fca091608798f52107f0b686e068ba89708900
elaineran <eyr4@cornell.edu>
2024-04-20 19:18:41 -0400

    fixed the aperturephotometry function

--- deepszsim/filters.py
+++ deepszsim/filters.py
@@ -4,7 +4,7 @@ submap filtering tools to analyze SZ submaps
 
 import numpy as np
 
-def get_tSZ_signal_aperture_photometry(dT_map, radmax_arcmin, 
+def get_tSZ_signal_aperture_photometry(dT_map, radmax_arcmin, pixel_scale
                                            fmax_arcmin=np.sqrt(2)):
     """
     Parameters:
@@ -12,6 +12,8 @@ def get_tSZ_signal_aperture_photometry(dT_map, radmax_arcmin,
     dT_map: array to represent the map in uK
     radmax_arcmin: float
         the radius of the inner aperture, in arcmin
+    pixel_scale: float
+        How many arcmin per pixel for the current settings
     fmax_arcmin: float
         fmax+radmax is the radius of the outer aperture, in arcmin
 
@@ -25,16 +27,14 @@ def get_tSZ_signal_aperture_photometry(dT_map, radmax_arcmin,
         thermal SZ effect signal
     """
 
-    center = np.array(dT_map.shape) / 2
+    radmax_pixels = radmax_arcmin / pixel_scale
+    radius_out_pixels = radmax_pixels * fmax_arcmin
+    
+    center = np.array(dT_map.shape) // 2
     x, y = np.indices(dT_map.shape)
     r = np.sqrt((x - center[0])**2 + (y - center[1])**2)
-
-    #define outer radius
-    radius_out=radmax_arcmin * fmax_arcmin 
-    #average in inner radius
-    disk_mean = dT_map[r < radmax_arcmin].mean() 
-    #average in outer radius
-    ring_mean = dT_map[(r >= radmax_arcmin) & (r < radius_out)].mean()
+        
+    disk_mean = dT_map[r < radmax_pixels].mean()
+    ring_mean = dT_map[(r >= radmax_pixels) & (r < radius_out_pixels)].mean()
     tSZ = disk_mean - ring_mean
-
     return disk_mean, ring_mean, tSZ
