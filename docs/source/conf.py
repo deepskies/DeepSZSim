26d9bc1f69ebd30227f4f716d943ad2f4bce4123
samueldmcdermott <samueldmcdermott@gmail.com>
2024-09-23 15:05:49 -0400

    updated `conf.py` for DeepSZSim details

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'DeepSZSim'
+copyright = '2024, DeepSkies'
+author = 'DeepSkies'
 
 release = '0.1'
 version = '0.1.0'
